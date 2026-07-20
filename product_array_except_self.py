770b7c39d6b56d21eef3dcbf464219c19bd650c7
Vedikgannoji <vedikgannoji5126@gmail.com>
2026-07-20 21:15:18 +0530

    product_array_except_self

---
+++ product_array_except_self.py
@@ -0,0 +1,13 @@
+nums = [1,2,3,4]
+def product_array_except_self(nums):
+    output=[1]*(len(nums))
+    prefix=1
+    for i in range(0,len(nums)):
+        output[i]=prefix
+        prefix *= nums[i]
+    postfix=1
+    for i in range(len(nums)-1,-1,-1):
+        output[i] *= postfix
+        postfix *= nums[i]
+    return output
+print(product_array_except_self(nums))

63eb4c60027ca8d3c4064329718d47332a576514
Vedikgannoji <vedikgannoji5126@gmail.com>
2026-07-20 20:42:33 +0530

    valid-palindrome

---
+++ valid_palindrome.py
@@ -0,0 +1,18 @@
+def is_palindrome(s):
+    l=0
+    r=len(s)-1
+    while l<r:
+        if not s[l].isalnum():
+            l+=1
+            continue
+        if not s[r].isalnum():
+            r-=1
+            continue
+        if s[l].lower()!=s[r].lower():
+            return False
+        l+=1
+        r-=1
+    return True
+            
+s="A man, a plan, a canal: Panama"
+print(is_palindrome(s))

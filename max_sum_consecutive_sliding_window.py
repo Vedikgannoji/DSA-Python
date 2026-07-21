b7e398d908aaff919b87ed5759ad20d9d8f4f3eb
Vedikgannoji <vedikgannoji5126@gmail.com>
2026-07-21 15:13:19 +0530

    cons_max_sum_sliding_window

---
+++ max_sum_consecutive_sliding_window.py
@@ -0,0 +1,11 @@
+nums = [4,2,1,7,8,1,2,8]
+k=3
+def cons_max_sum(nums,k):
+    sum_window=sum(nums[0:k])
+    max_sum=sum_window
+    for i in range(k,len(nums)):
+        sum_window+=nums[i]
+        sum_window-=nums[i-k]
+        max_sum=max(max_sum,sum_window)
+    return max_sum
+print(cons_max_sum(nums,k))

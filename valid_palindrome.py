74cc39abb20880306fdebdabaad3711c11d504f2
Vedikgannoji <vedikgannoji5126@gmail.com>
2026-07-21 17:32:54 +0530

    updated valid_palindrome

--- valid_palindrome.py
+++ valid_palindrome.py
@@ -2,13 +2,13 @@ def is_palindrome(s):
     l=0
     r=len(s)-1
     while l<r:
-        while not s[l].isalnum():
+        while l<r and not s[l].isalnum():
             l+=1
             continue
-        while not s[r].isalnum():
+        while l<r and not s[r].isalnum():
             r-=1
             continue
-        while s[l].lower()!=s[r].lower():
+        if s[l].lower()!=s[r].lower():
             return False
         l+=1
         r-=1

95878a7d5fec85c034c0315e7bd49274b1ba228d
Vedikgannoji <vedikgannoji5126@gmail.com>
2026-07-21 16:39:34 +0530

    opp_direction_2pointer_template

---
+++ opp_direction_2pointer_template.py
@@ -0,0 +1,17 @@
+#opp direction two pointers template
+#ex. two sum II, palindrome, reverse string, container with most water
+
+left=0
+right=len(nums)-1
+
+while start<end:
+    #process
+    if (condition): 
+        left +=1
+    else:
+        right -=1
+
+
+
+        
+    

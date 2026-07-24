604f19719b00cf4e33073726745f9a5a2c7fe212
Vedikgannoji <vedikgannoji5126@gmail.com>
2026-07-24 12:59:02 +0530

    move_zeroes

--- move_zeroes.py
+++ move_zeroes.py
@@ -3,4 +3,13 @@ nums=[0,1,0,3,12]
 def move_zeroes(nums):
     read=0
     write=0
+    while read<len(nums):
+        if nums[read]!=0:
+            nums[write]=nums[read]
+            write+=1
+        read+=1
+   
+    return nums
+print(move_zeroes(nums))
+
 

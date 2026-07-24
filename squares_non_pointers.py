e43e6d72677a036146048d8ca7b3557197e65efb
Vedikgannoji <vedikgannoji5126@gmail.com>
2026-07-24 19:44:01 +0530

    squares_non_pointers

---
+++ squares_non_pointers.py
@@ -0,0 +1,10 @@
+##Leetcode 977 Squares of a Sorted Array-without two pointers
+nums = [-4,-1,0,3,10]
+def squares(nums):
+    res=[]
+    for i in nums:
+        res.append[i*i]
+    res.sort()
+    return res
+print(squares(nums))
+    

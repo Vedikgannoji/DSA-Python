f5d82631ea1f1e1ed2c3177ccf8dc42bf139c1a4
Vedikgannoji <vedikgannoji5126@gmail.com>
2026-07-24 13:22:33 +0530

    updated_remove_duplicates

--- remove_duplicates.py
+++ remove_duplicates.py
@@ -1,4 +1,4 @@
-#LeetCode 26 remove duplicates from sorted array
+#remove duplicates from sorted array
 nums = [0,0,1,1,1,2,2,3,3,4]
 def remove_duplicates(nums):
     read=1
@@ -9,7 +9,10 @@ def remove_duplicates(nums):
             write+=1
         read+=1
     while write<len(nums):
-        nums.remove(nums[write])
+        nums.remove[nums[write]]
+        #use this when asked to remove duplicates completely
 
     return nums
+    return nums[:write] #use this when asked just to return non-duplicates
+
 print(remove_duplicates(nums))

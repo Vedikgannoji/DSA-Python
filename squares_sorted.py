3e8cdda10622c789668f82bcd2b36470bcab7c52
Vedikgannoji <vedikgannoji5126@gmail.com>
2026-07-24 19:41:24 +0530

    updated_squares_sorted

--- squares_sorted.py
+++ squares_sorted.py
@@ -16,6 +16,20 @@ print(squares(nums))
 '''
 #optimized
 nums = [-4,-1,0,3,10]
+ans = [0] * len(nums)
 def squares(nums):
-    
+    left=0
+    right=len(nums)-1
+    write=len(nums)-1
+    while left<=right:
+        if abs(nums[left])>abs(nums[right]):
+            ans[write]=nums[left] ** 2
+            left+=1
+        else:
+            ans[write]=nums[right] ** 2
+            right-=1
+        write-=1
+    return ans
+print(squares(nums))
+
 

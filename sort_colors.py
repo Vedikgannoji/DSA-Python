35924ec8351b485a0b57d7cb83c4c4b1d5e94dc1
Vedikgannoji <vedikgannoji5126@gmail.com>
2026-07-24 18:12:14 +0530

    sort_colors

--- sort_colors.py
+++ sort_colors.py
@@ -2,6 +2,20 @@
 
 nums = [2,0,2,1,1,0]
 def sort_colors(nums):
-    
-            
-        
+    low = 0
+    mid = 0
+    high = len(nums) - 1
+    while mid<=high:
+        if nums[mid]==0:
+            nums[low],nums[mid]=nums[mid],nums[low]
+            low+=1
+            mid+=1
+        elif nums[mid]==1:
+            mid+=1
+        else:
+            nums[high],nums[mid]=nums[mid],nums[high]
+            high-=1
+            mid+=1
+
+
+

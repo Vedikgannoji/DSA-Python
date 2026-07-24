b23d379a7ca7e60a670d4fd32028c35b16dbdee3
Vedikgannoji <vedikgannoji5126@gmail.com>
2026-07-24 19:16:26 +0530

    brute_force_squares_sorted

---
+++ squares_sorted.py
@@ -0,0 +1,14 @@
+#Leetcode 977 Squares of a Sorted Array
+#bruteforce-O(nlogn) tc
+#squaring-O(n) Sorting O(nlogn)
+nums = [-4,-1,0,3,10]
+def squares(nums):
+    read=0
+    write=0
+    while read<len(nums):
+        nums[write]=(nums[read]**2)
+        write+=1
+        read+=1
+    nums.sort()
+    return nums
+print(squares(nums))

459713e013be6d90ee2b0ab597c10608d5b2eb54
Vedikgannoji <vedikgannoji5126@gmail.com>
2026-07-24 19:08:21 +0530

    updated_valid_triangle

--- valid_triangle.py
+++ valid_triangle.py
@@ -1,1 +1,16 @@
 # Leetcode 611 Valid Triangle
+nums=[2,2,3,4]
+def valid_triangle(nums):
+    nums.sort()
+    count=0
+    for i in range(len(nums)-1,1,-1):
+        left=0
+        right=i-1
+        while left<right:
+            if nums[left]+nums[right]>nums[i]:
+                count += (right-left)
+                right-=1
+            else:
+                left+=1  
+    return count
+print(valid_triangle(nums))

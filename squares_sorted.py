e0ba6c816f951b6e36e7d245ca0ea36465c4eeb8
Vedikgannoji <vedikgannoji5126@gmail.com>
2026-07-24 19:21:00 +0530

    updated_squares_sorted

--- squares_sorted.py
+++ squares_sorted.py
@@ -1,6 +1,7 @@
 #Leetcode 977 Squares of a Sorted Array
 #bruteforce-O(nlogn) tc
 #squaring-O(n) Sorting O(nlogn)
+'''
 nums = [-4,-1,0,3,10]
 def squares(nums):
     read=0
@@ -12,3 +13,9 @@ def squares(nums):
     nums.sort()
     return nums
 print(squares(nums))
+'''
+#optimized
+nums = [-4,-1,0,3,10]
+def squares(nums):
+    
+

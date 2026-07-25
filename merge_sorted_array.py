9dcdeb6bd4aa4144641588ada0d8fc321f4787d1
Vedikgannoji <vedikgannoji5126@gmail.com>
2026-07-25 20:30:05 +0530

    merge_sorted_array

--- merge_sorted_array.py
+++ merge_sorted_array.py
@@ -1,4 +1,4 @@
-#Leetcode 88 Merge Sorted Array Bruteforce
+#Leetcode 88 Merge Sorted Array
 
 nums1 = [1,2,3,0,0,0]
 m=3
@@ -6,11 +6,4 @@ nums2 = [2,5,6]
 n=3
 
 def merge(nums1,m,nums2,n):
-    total=m+n
-    j=0
-    for i in range(m,total):
-        nums1[i]=nums2[j]
-        j+=1
-    nums1.sort()
-    return nums1
-print(merge(nums1,m,nums2,n))
+    

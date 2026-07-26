3c19b392a9964210e2daa94c11a41052cdbcf988
Vedikgannoji <vedikgannoji5126@gmail.com>
2026-07-26 21:24:49 +0530

    richest_customer_wealth

--- richest_customer_wealth.py
+++ richest_customer_wealth.py
@@ -1,1 +1,8 @@
 #Leetcode 1672 Richest Customer Wealth
+accounts = [[1,5],[7,3],[3,5]]
+def richCustomer(accounts):
+    total=[]
+    for account in accounts:
+        total.append(sum(account))
+    return max(total)
+print(richCustomer(accounts))

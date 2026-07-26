4d912de6c29c4d3c0380add04189d87a9d7ac8dd
Vedikgannoji <vedikgannoji5126@gmail.com>
2026-07-26 16:06:07 +0530

    jewels_stones_hashset

---
+++ jewels_stones_hashset.py
@@ -0,0 +1,12 @@
+#Leetcode 771 Stones & Jewels Hashset
+jewels = "aA"
+stones = "aAAbbbb"
+
+def jewels_stones(jewels,stones):
+    jewel_set=set(jewels)
+    res=0
+    for stone in stones:
+        if stone in jewel_set:
+            res+=1
+    return res
+print(jewels_stones(jewels,stones))

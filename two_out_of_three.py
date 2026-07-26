fe343555c900909f1df20d1d2f07a18df5ec344d
Vedikgannoji <vedikgannoji5126@gmail.com>
2026-07-26 20:26:59 +0530

    updated two_out_of_three

--- two_out_of_three.py
+++ two_out_of_three.py
@@ -11,3 +11,19 @@ def two_out_of_three(nums1,nums2,nums3):
 
     return list((s1 & s2) | (s1 & s3) | (s2 & s3))
 print(two_out_of_three(nums1,nums2,nums3))
+
+#using hashmaps
+def two_out_of_three(nums1,nums2,nums3):
+    res=[]
+    freq={}
+    for num in set(nums1):
+        freq[num]=freq.get(num,0)+1
+    for num in set(nums2):
+            freq[num]=freq.get(num,0)+1
+    for num in set(nums3):
+            freq[num]=freq.get(num,0)+1
+    for num in freq:
+        if freq[num]>=2:
+            res.append(num)
+    return res
+print(two_out_of_three(nums1,nums2,nums3))

dd840c74ee445eda568ea9eeb2510c51c35f6ad7
Vedikgannoji <vedikgannoji5126@gmail.com>
2026-07-29 22:08:08 +0530

    plus_one

--- plus_one.py
+++ plus_one.py
@@ -3,4 +3,9 @@ digits = [1,2,3]
 def plusOne(digits):
     n = len(digits)
     for i in range(n - 1, -1, -1):
-        
+        if digits[i] < 9:
+            digits[i] += 1
+            return digits
+    return [1] + digits
+
+print(plusOne(digits))

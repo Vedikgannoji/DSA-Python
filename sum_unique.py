c228fe9b6cae7a47d0f611fd5a04bd92bd9b01b7
Vedikgannoji <vedikgannoji5126@gmail.com>
2026-07-30 21:49:59 +0530

    sum_unique

--- sum_unique.py
+++ sum_unique.py
@@ -5,8 +5,8 @@ def sumUnique(nums):
     total=0
     for num in nums:
         freq[num]=freq.get(num,0)+1
-    for num in freq:
-        if freq[num]==1:
-            new.append(num)
+    for num,count in freq.items():
+        if count==1:
+            
     return sum(new)
 print(sumUnique(nums))

aa01619fb3cdff9cc6620ce71d48566786e399a5
Vedikgannoji <vedikgannoji5126@gmail.com>
2026-07-30 21:48:44 +0530

    sum_unique

---
+++ sum_unique.py
@@ -0,0 +1,12 @@
+#Leetcode 1748 Sum of Unique elements
+nums=[1,2,3,2]
+def sumUnique(nums):
+    freq={}
+    new=[]
+    for num in nums:
+        freq[num]=freq.get(num,0)+1
+    for num in freq:
+        if freq[num]==1:
+            new.append(num)
+    return sum(new)
+print(sumUnique(nums))

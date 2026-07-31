dfbcaf9b52d7913556e66e33650002bc74ffd83c
Vedikgannoji <vedikgannoji5126@gmail.com>
2026-07-31 18:01:30 +0530

    length_last_wrd

--- length_last_wrd.py
+++ length_last_wrd.py
@@ -2,11 +2,14 @@
 s = "Hello World"
 def lengthLastWord(s):
     count=0
+    started=False #to ensuure code works if space is at the end
     for ch in range(len(s)-1,-1,-1):
-        if s[ch]!=" ":
-            count+=1
+        if s[ch]==" ":
+            if started:
+                break
         else:
-            break
+            started=True
+            count+=1
     return count
 
 print(lengthLastWord(s))

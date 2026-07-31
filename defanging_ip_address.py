f8e7c69ad6fe6d14ec7be10d50eb0e25c0b20655
Vedikgannoji <vedikgannoji5126@gmail.com>
2026-07-31 17:48:19 +0530

    defanging_ip_address

--- defanging_ip_address.py
+++ defanging_ip_address.py
@@ -1,8 +1,11 @@
 #1108 Defanging IP Address
 address = "255.100.50.0"
 def defanging(address):
+    new=""
     for ch in address:
-        if ch=='.':
-            ch.replace(".","[.]")
-    return address
+        if ch!='.':
+            new=new+ch
+        else:
+            new=new+"[.]"
+    return new
 print(defanging(address))

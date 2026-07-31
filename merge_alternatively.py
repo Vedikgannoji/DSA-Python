f26ce90f17fb00dd317d24d65ca3883a66c1c377
Vedikgannoji <vedikgannoji5126@gmail.com>
2026-07-31 19:41:08 +0530

    merge_alternatively

--- merge_alternatively.py
+++ merge_alternatively.py
@@ -12,3 +12,15 @@ def mergeAlternatively(word1,word2):
     return res
     
 print(mergeAlternatively(word1,word2))
+
+#using join- use a list initially and join as string for output as strings are immutable and create a new string for every operation.
+def mergeAlternatively2(word1,word2):
+    res=[]
+    limit=min(len(word1),len(word2))
+    for i in range(limit):
+        res.append(word1[i])
+        res.append(word2[i])
+    res.append(word1[limit:])
+    res.append(word2[limit:])
+    return "".join(res)
+print(mergeAlternatively2(word1,word2))

4df61aa8933b731b5e92d934f81bfb791b297acc
Vedikgannoji <vedikgannoji5126@gmail.com>
2026-08-01 20:12:35 +0530

    split_string_balanced

--- split_string_balanced.py
+++ split_string_balanced.py
@@ -13,7 +13,26 @@ def splitString(s):
             count+=1
             r=0
             l=0
-        
+
+    return count
+
+print(splitString(s))
+
+#optimised
+def splitString(s):
+    count=0
+    r=0
+    l=0
+    for ch in range(0,len(s)):
+        if s[ch]=='R':
+            r+=1
+        else:
+            l+=1
+        if r!=0 and l!=0 and r==l:
+            count+=1
+            r=0
+            l=0
+
     return count
 
 print(splitString(s))

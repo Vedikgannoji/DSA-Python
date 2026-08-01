d870e9ceaafccbcf141ca06dc10e9c28e194acbf
Vedikgannoji <vedikgannoji5126@gmail.com>
2026-08-01 20:47:36 +0530

    find_difference

--- find_difference.py
+++ find_difference.py
@@ -2,8 +2,13 @@
 s = "abcd"
 t = "abcde"
 def difference(s,t):
+    freq={}
+    for ch in s:
+        freq[ch]=freq.get(ch,0)+1
+
     for ch in t:
-        if ch not in s:
+        freq[ch]=freq.get(ch,0)-1
+        if freq[ch]<0:
             return ch
 
 print(difference(s,t))

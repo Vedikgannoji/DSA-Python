eb1cac60ee0deeebbd9bc781b829a8a2d3b048b8
Vedikgannoji <vedikgannoji5126@gmail.com>
2026-08-02 14:55:05 +0530

    contains_duplicate_II

--- contains_duplicate_II.py
+++ contains_duplicate_II.py
@@ -1,3 +1,13 @@
 # 129 Contains Duplicate II
 nums = [1,2,3,1]
 k = 3
+def contains_duplicates(nums,k):
+    last_index={}
+    for i in range(0,len(nums)):
+        if nums[i] in last_index:
+            if i-last_index[nums[i]] <=k:
+                return True
+        last_index[nums[i]]=i
+    return False
+
+print(contains_duplicates(nums,k))

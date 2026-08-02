de3e843a8b7016e5c312a4a712b39e7fa079b784
Vedikgannoji <vedikgannoji5126@gmail.com>
2026-08-02 14:13:01 +0530

    longest_common_prefix

---
+++ longest_common_prefix.py
@@ -0,0 +1,13 @@
+#14 Longest Common Prefix 
+
+strs = ["flower","flow","flight"]
+def longestCommonPrefix(strs):
+    prefix=""
+    n=len(min(strs,key=len))
+    for i in range(0,n):
+        for word in strs:
+            if word[i]!=strs[0][i]:
+                return prefix
+        prefix+=strs[0][i]
+    return prefix
+print(longestCommonPrefix(strs))

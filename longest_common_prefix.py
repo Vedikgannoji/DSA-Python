3e89ef50b07e048b215f49051cd46840d71ad864
Vedikgannoji <vedikgannoji5126@gmail.com>
2026-08-02 14:13:25 +0530

    longest_common_prefix

--- longest_common_prefix.py
+++ longest_common_prefix.py
@@ -4,10 +4,6 @@ strs = ["flower","flow","flight"]
 def longestCommonPrefix(strs):
     prefix=""
     n=len(min(strs,key=len))
-    for i in range(0,n):
-        for word in strs:
-            if word[i]!=strs[0][i]:
-                return prefix
-        prefix+=strs[0][i]
+    
     return prefix
 print(longestCommonPrefix(strs))

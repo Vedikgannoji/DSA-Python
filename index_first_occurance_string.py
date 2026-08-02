8479d109faa3ff4df0de9291ace9817a9aa15eae
Vedikgannoji <vedikgannoji5126@gmail.com>
2026-08-02 14:28:44 +0530

    index_first_occurance_string

--- index_first_occurance_string.py
+++ index_first_occurance_string.py
@@ -1,2 +1,11 @@
 #28 Index of the First Occurrence in a String
 
+haystack = "sadbutsad"
+needle = "sad"
+
+def firstOccurance(haystack,needle):
+    for i in range(len(haystack)-len(needle)+1):
+        if haystack[i:i+len(needle)]==needle:
+            return i
+    return -1
+print(firstOccurance(haystack,needle))

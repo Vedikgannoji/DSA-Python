cf6c34bddad59669d9b71fcfc1e97db902c2fb63
Vedikgannoji <vedikgannoji5126@gmail.com>
2026-08-03 19:39:05 +0530

    stone_game_3

--- stone_game_3.py
+++ stone_game_3.py
@@ -1,3 +1,22 @@
 # LeetCode 1406 – Stone Game III
 stoneValue = [1,2,3,7]
 def stoneGame(stoneValue):
+    n = len(stoneValue)
+
+    dp = [0] * (n + 1)
+
+    for i in range(n - 1, -1, -1):
+        take = 0
+        dp[i] = float('-inf')
+
+        for k in range(3):
+            if i + k < n:
+                take += stoneValue[i + k]
+                dp[i] = max(dp[i], take - dp[i + k + 1])
+
+    if dp[0] > 0:
+        return "Alice"
+    elif dp[0] < 0:
+        return "Bob"
+    else:
+        return "Tie"

6ec690297996005b8fd3c7a2cce77ea072a8f2bb
Vedikgannoji <vedikgannoji5126@gmail.com>
2026-08-03 19:39:37 +0530

    stone_game_3.py

--- stone_game_3.py
+++ stone_game_3.py
@@ -14,9 +14,4 @@ def stoneGame(stoneValue):
                 take += stoneValue[i + k]
                 dp[i] = max(dp[i], take - dp[i + k + 1])
 
-    if dp[0] > 0:
-        return "Alice"
-    elif dp[0] < 0:
-        return "Bob"
-    else:
-        return "Tie"
+   

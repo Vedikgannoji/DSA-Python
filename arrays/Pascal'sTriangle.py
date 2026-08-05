b5e31ef49e295a78a5f702fb7e70e8069241e087
Vedikgannoji <vedikgannoji5126@gmail.com>
2026-08-05 21:13:47 +0530

    Pascal'sTriangle

--- arrays/Pascal'sTriangle.py
+++ arrays/Pascal'sTriangle.py
@@ -1,14 +1,18 @@
 #118
 numRows = 5
 def pascalTriangle(numRows):
-    res=[]
-    res.append([1])
-    res.append([1,1])
-    for i in range(2,numRows):
+    if numRows==1:
+        return [[1]]
+    else:
+
+        res=[]
         res.append([1])
-        for j in range(len(res[i-1])-1):
-            res[i].append(res[i-1][j]+res[i-1][j+1])
-        res[i].append(1)
+        res.append([1,1])
+        for i in range(2,numRows):
+            res.append([1])
+            for j in range(len(res[i-1])-1):
+                res[i].append(res[i-1][j]+res[i-1][j+1])
+            res[i].append(1)
     return res
 print(pascalTriangle(numRows))
 
